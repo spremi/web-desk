7b3c05c20aeaa192555b9c6004dff03f5f359012
Sanjeev Premi <spremi@ymail.com>
2020-11-01 17:28:13 +0530

    chore: app: Add route /home
    Command:
      ng g c routes/home --style=sass --lintFix=true
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
