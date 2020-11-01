7b71f00923f6088de701bed1567ee2ec7efd612f
Sanjeev Premi <spremi@ymail.com>
2020-11-01 16:58:09 +0530

    chore: project: Create via 'ng new'
    Command:
      ng new web-desk --prefix=sp --routing=true --style=sass
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'web-desk';
+}
