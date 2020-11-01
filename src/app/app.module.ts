4e9ce2bd6707c339487e2f0fd4f89946c11d1ba3
Sanjeev Premi <spremi@ymail.com>
2020-11-01 17:32:35 +0530

    chore: app: Add part 'header'
    Command:
      ng g c parts/header --style=sass --lintFix=true
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './parts/header/header.component';
 import { AboutComponent } from './routes/about/about.component';
 import { HomeComponent } from './routes/home/home.component';
 
@@ -12,6 +13,7 @@ import { HomeComponent } from './routes/home/home.component';
     AppComponent,
     HomeComponent,
     AboutComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
