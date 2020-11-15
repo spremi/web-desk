3d1bdda0e8e873626726d261927d61e6f3800352
Sanjeev Premi <spremi@ymail.com>
2020-11-16 02:43:06 +0530

    feat: svc: previous-route: Initial implementation
    Tap the NavigationEnd event to track the previous route.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/services/previous-route.service.ts
+++ src/app/services/previous-route.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PreviousRouteService {
 
-  constructor() { }
+  private prevRoute: string;
+  private currRoute: string;
+
+  constructor(private router: Router) {
+    this.currRoute = this.router.url;
+    this.prevRoute = null;
+
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+      this.prevRoute = this.currRoute;
+      this.currRoute = event.urlAfterRedirects;
+    });
+  }
+
+  public get(): string {
+    return this.prevRoute;
+  }
 }
