9a8269ddf44cdcd29dd849ee2167b75ee1396a79
Sanjeev Premi <spremi@ymail.com>
2020-11-16 22:19:24 +0530

    feat: models: Define VersionInfo
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/version-info.ts
+++ src/app/models/version-info.ts
@@ -1,2 +1,14 @@
+//
+// @project     web-desk
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+
 export interface VersionInfo {
+  chrome: string;
+  node: string;
+  electron: string;
+  package: string;
 }
