f28a6449d561f4888bf5084c0dca79c65262f298
Sanjeev Premi <spremi@ymail.com>
2020-11-16 22:56:40 +0530

    feat: electron: Expose 'ipc' api via contextBridge
    contextBridge requires 'contextIsolation' to be enabled
    in webPreferences.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- electron/src/preload.ts
+++ electron/src/preload.ts
@@ -11,6 +11,9 @@
 // The process runs in the same sandbox as a Chrome Extension.
 //
 
+import { contextBridge, ipcRenderer } from 'electron';
+import { IpcRequest, IPC_E2NG, IPC_NG2E } from './models/ipc-request';
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector: string, text: string) => {
     const element = document.getElementById(selector);
@@ -26,3 +29,38 @@ window.addEventListener('DOMContentLoaded', () => {
     replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
   }
 });
+
+/**
+ * Send requests from Angular application.
+ */
+function ipcSend(channel: string, data: IpcRequest): void {
+  if (channel.startsWith(IPC_NG2E)) {
+    //
+    // Map to common handler (ignoring sequence number).
+    //
+    ipcRenderer.send(IPC_NG2E, data);
+  } else {
+    console.log('[E] Unknown IPC channel (send) - ' + channel + '.');
+  }
+}
+
+/**
+ * Receive response from Electron.
+ */
+function ipcReceive(channel: string, func: () => void): void {
+  if (channel.startsWith(IPC_E2NG)) {
+    ipcRenderer.once(channel, func);
+  } else {
+    console.log('[E] Unknown IPC channel (receive) - ' + channel + '.');
+  }
+}
+
+//
+// Expose custom 'ipc' object via 'window' object.
+//
+contextBridge.exposeInMainWorld(
+  'ipc', {
+    send: ipcSend,
+    receive: ipcReceive,
+    }
+);
