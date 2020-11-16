311f83c46722d3b7251f0230ae0e4e9f00da5286
Sanjeev Premi <spremi@ymail.com>
2020-11-17 00:34:16 +0530

    feat: models: Define DeskConfig
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/desk-config.ts
+++ src/app/models/desk-config.ts
@@ -1,2 +1,32 @@
+//
+// @project     web-desk
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+
+
+/**
+ * Describes a web application.
+ */
+export interface DeskApp {
+  label: string;
+  url: string;
+}
+
+/**
+ * Describes stored configuration.
+ */
 export interface DeskConfig {
+  apps: DeskApp[];
+}
+
+/**
+ * Initializer for DeskConfig
+ */
+export function initDeskConfig(): DeskConfig {
+  return {
+    apps: [],
+  };
 }
