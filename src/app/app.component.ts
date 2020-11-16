ccfc9c886bb0d83bf60e28b48858fab24dc87434
Sanjeev Premi <spremi@ymail.com>
2020-11-17 00:22:41 +0530

    feat: app: Dummy message exchange with electron
    Just to confirm that IPC mechanism is working well.
    Test message exchange for primitive data types and
    object.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initIpcRequest } from '@models/ipc-request';
+import { IpcService } from '@services/ipc.service';
 import { PreviousRouteService } from '@services/previous-route.service';
+
+interface TestMessage {
+  msg: string;
+}
 
 @Component({
   selector: 'sp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'web-desk';
 
-  constructor(private prevRouteSvc: PreviousRouteService) { }
+  constructor(private prevRouteSvc: PreviousRouteService,
+              private ipcSvc: IpcService
+  ) { }
+
+  ngOnInit(): void {
+    const testString = initIpcRequest('test-string');
+
+    this.ipcSvc.send<string>(testString).then(result => {
+      if (result) {
+        console.log('IPC success. Result = ' + result);
+      } else {
+        console.log('IPC failure.');
+      }
+    }).catch(err => {
+        console.log(err);
+    });
+
+    const testObject = initIpcRequest('test-object');
+
+    this.ipcSvc.send<TestMessage>(testObject).then(result => {
+      if (result) {
+        console.log('IPC success. Result = ' + JSON.stringify(result));
+      } else {
+        console.log('IPC failure.');
+      }
+    }).catch(err => {
+        console.log(err);
+    });
+  }
 }
