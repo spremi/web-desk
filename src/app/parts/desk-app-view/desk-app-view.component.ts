bf0c771ae4c64aea1503f1bb042457747276c201
Sanjeev Premi <spremi@ymail.com>
2021-01-01 02:28:55 +0530

    feat: parts: desk-app-view: Subscribe to desk-app events
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/desk-app-view/desk-app-view.component.ts
+++ src/app/parts/desk-app-view/desk-app-view.component.ts
@@ -1,19 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
-import { DeskApp } from '@models/desk-config';
+import { DeskApp, DeskAppEvent } from '@models/desk-config';
+import { IpcService } from '@services/ipc.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sp-desk-app-view',
   templateUrl: './desk-app-view.component.html',
   styleUrls: ['./desk-app-view.component.sass'],
 })
-export class DeskAppViewComponent implements OnInit {
+export class DeskAppViewComponent implements OnInit, OnDestroy {
 
   @Input() data: DeskApp;
 
-  constructor() { }
+  private sub: Subscription;
+
+  constructor(private ipcSvc: IpcService) { }
 
   ngOnInit(): void {
+    this.sub = this.ipcSvc.getAppEvents().subscribe((ev: DeskAppEvent) => {
+      console.log('Received event "' + ev.event + '" from desk app ' + ev.aid);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
   onOptions(ev: MouseEvent): void {
