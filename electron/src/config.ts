49161af13df345e20b6528628f2d75b0acf8889f
Sanjeev Premi <spremi@ymail.com>
2021-01-02 23:25:50 +0530

    feat: electron: config: CRUD methods for desk app
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- electron/src/config.ts
+++ electron/src/config.ts
@@ -10,10 +10,12 @@
 // Application level configuration.
 //
 
+import { v4 as uuidV4 } from 'uuid';
+
 import * as fs from 'fs';
 import * as path from 'path';
 
-import { initDeskConfig, DeskConfig } from './models/desk-config';
+import { initDeskApp, initDeskConfig, DeskApp, DeskConfig } from './models/desk-config';
 
 /**
  * Directory containing the configuration.
@@ -89,4 +91,107 @@ export function ensureConfig(): void {
       console.log('[E] : ' + e.name + ' ' + e.message);
     }
   }
+}
+
+/**
+ * Add desk application to configuration.
+ */
+export function addDeskApp(
+  gid: string,
+  label: string,
+  url: string): DeskApp {
+    const cfg = readConfig();
+
+    if (!cfg) {
+      return null;
+    }
+
+    const aid = uuidV4();
+    const seq = cfg.seq++;
+
+    const app = initDeskApp(aid, gid, seq, label, url);
+
+    cfg.apps.push(app);
+
+    if (saveConfig(cfg)) {
+      return app;
+    }
+
+    return null;
+}
+
+/**
+ * Get configuration of desk application with specified id.
+ */
+export function getDeskApp(id: string): DeskApp {
+  const cfg = readConfig();
+
+  if (!cfg) {
+    return null;
+  }
+
+  const i = cfg.apps.findIndex((elem) =>
+                elem !== null && typeof elem === 'object' && elem.aid === id);
+
+  let app: DeskApp = null;
+
+  if (i !== -1) {
+    app = { ... cfg.apps[i] };
+  }
+
+  return app;
+}
+
+/**
+ * Modify desk application to configuration.
+ *
+ * TODO: Re-order if 'seq' changes. For now, simply save as-is.
+ */
+export function modDeskApp(
+  gid: string,
+  aid: string,
+  seq: number,
+  label: string,
+  url: string): DeskApp {
+    const cfg = readConfig();
+
+    if (!cfg) {
+      return null;
+    }
+
+    const i = cfg.apps.findIndex((elem) =>
+                  elem !== null && typeof elem === 'object' && elem.aid === aid);
+
+    if (i === -1) {
+      return null;
+    }
+
+    cfg.apps[i].gid = gid;
+    cfg.apps[i].seq = seq;
+    cfg.apps[i].label = label;
+    cfg.apps[i].url = url;
+
+    if (saveConfig(cfg)) {
+      return getDeskApp(aid);
+    }
+
+    return null;
+}
+
+/**
+ * Delete desk application to configuration.
+ */
+export function delDeskApp(aid: string): boolean {
+  const cfg = readConfig();
+
+  if (!cfg) {
+    return null;
+  }
+
+  const apps = cfg.apps.filter((elem) =>
+                elem !== null && typeof elem === 'object' && elem.aid !== aid);
+
+  cfg.apps = apps;
+
+  return saveConfig(cfg);
 }
