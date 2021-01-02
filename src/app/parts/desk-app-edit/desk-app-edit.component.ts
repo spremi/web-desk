2b90021f8f80db1239ef48f8eda051787befc799
Sanjeev Premi <spremi@ymail.com>
2021-01-02 17:25:13 +0530

    feat: parts: desk-app-edit: Basic implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/desk-app-edit/desk-app-edit.component.ts
+++ src/app/parts/desk-app-edit/desk-app-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { DeskApp } from '@models/desk-config';
+import { initIpcRequest, IpcNg2E, IpcRequest } from '@models/ipc-request';
+import { IpcService } from '@services/ipc.service';
 
 @Component({
   selector: 'sp-desk-app-edit',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeskAppEditComponent implements OnInit {
 
-  constructor() { }
+  appForm: FormGroup;
+
+  @Input() data: DeskApp;
+
+  constructor(private ipcSvc: IpcService) { }
 
   ngOnInit(): void {
+    this.appForm = new FormGroup({
+      label: new FormControl((this.data && this.data.label ? this.data.label : ''), {
+        validators: Validators.required,
+      }),
+      url: new FormControl((this.data && this.data.url ? this.data.url : ''), {
+        validators: Validators.required,
+      }),
+    });
   }
 
+  onSubmit(): void {
+    const DEFAULT_GID = '0';
+
+    if (this.appForm.valid) {
+      this.appForm.disable();
+
+      let cmd: IpcRequest;
+      const params: string[] = [];
+
+      if (this.data === null) {
+        cmd = initIpcRequest(IpcNg2E.APP_CREATE);
+
+        params.push(DEFAULT_GID);
+      } else {
+        cmd = initIpcRequest(IpcNg2E.APP_MODIFY);
+
+        params.push(this.data.gid);
+        params.push(this.data.aid);
+        params.push(this.data.seq.toString());
+      }
+
+      params.push(this.appForm.get('label').value);
+      params.push(this.appForm.get('url').value);
+
+      cmd.reqParams = params;
+
+      this.ipcSvc.send<DeskApp>(cmd).then(result => {
+        if (!result) {
+          console.log('IPC failure save.');
+        }
+
+        this.appForm.reset();
+      }).catch(err => {
+          console.log(err);
+
+          this.appForm.enable();
+      });
+    }
+  }
 }
