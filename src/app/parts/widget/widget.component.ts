d9cb0002748e9f852ff5045f3ff98a0a4b14c123
Sanjeev Premi <spremi@ymail.com>
2021-01-09 10:56:16 +0530

    feat: parts: widget: Borrow code from 'desk-app-view'
    The original plan to use same component in 'widget' and
    'expanded' form appear to be cumbersome.
    
    There are, naturally, some improvements in the borrowed
    the borrowed code.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/widget/widget.component.ts
+++ src/app/parts/widget/widget.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { DeskApp, DeskAppEvent } from '@models/desk-config';
+import { IpcService } from '@services/ipc.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sp-widget',
   templateUrl: './widget.component.html',
   styleUrls: ['./widget.component.sass'],
 })
-export class WidgetComponent implements OnInit {
+export class WidgetComponent implements OnInit, OnDestroy {
+  private subs: Subscription[] = [];
 
-  constructor() { }
+  @Input() app: DeskApp;
+
+  constructor(private ipcSvc: IpcService) { }
 
   ngOnInit(): void {
+    const subEvents = this.ipcSvc.getAppEvents().subscribe((ev: DeskAppEvent) => {
+      console.log('Received event "' + ev.event + '" from desk app ' + ev.aid);
+    });
+
+    this.subs.push(subEvents);
   }
 
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => {
+      if (sub) {
+        sub.unsubscribe();
+      }
+    });
+  }
+
+  onDetails(): void {
+    console.log('TODO: Show details.');
+  }
+
+  onLaunch(): void {
+    console.log('TODO: Launch application.');
+  }
 }
