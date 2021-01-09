12960774f7bfeee88f193c93884091818efb963a
Sanjeev Premi <spremi@ymail.com>
2021-01-09 17:38:15 +0530

    feat: app: models: Define run-time state
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/app-state.ts
+++ src/app/models/app-state.ts
@@ -1,2 +1,57 @@
-export interface AppState {
+//
+// @project     web-desk
+//
+// @author      Sanjeev Premi <spremi@ymail.com>
+//
+// @license     BSD-3-Clause
+//
+
+
+/**
+ * Run-time attributes of an application.
+ */
+export interface RuntimeAttrs {
+  /** Application is running / stopped. */
+  isRunning: boolean;
+
+  /** Application is minimized / visible. */
+  isMinimized: boolean;
+}
+
+/**
+ * Describes map of application-id running applications.
+ */
+export interface AppRunAttrs {
+  [aid: string]: RuntimeAttrs;
+}
+
+/**
+ * Describes Unified app application state.
+ */
+export interface AppRunState {
+  /** User can edit application? */
+  canEdit: boolean;
+
+  /** Map of running applications. */
+  runApps: AppRunAttrs;
+}
+
+/**
+ * Initializer for RuntimeAttrs.
+ */
+export function initRuntimeAttrs(run?: boolean, min?: boolean): RuntimeAttrs {
+  return {
+    isRunning: run ? run : false,
+    isMinimized: min ? min : false,
+  };
+}
+
+/**
+ * Initializer for AppRunState.
+ */
+export function initAppRunState(): AppRunState {
+  return {
+    canEdit: false,
+    runApps: {} as AppRunAttrs,
+  };
 }
