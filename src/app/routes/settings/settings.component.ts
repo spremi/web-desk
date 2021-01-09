027096242eabbd4aed1ce0c821679392bfc486ed
Sanjeev Premi <spremi@ymail.com>
2021-01-10 00:17:54 +0530

    feat: /settings: Propagate 'canEdit'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/routes/settings/settings.component.ts
+++ src/app/routes/settings/settings.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { Router } from '@angular/router';
+import { RunStateService } from '@services/run-state.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sp-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.sass'],
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, OnDestroy {
+  /**
+   * Array of subscriptions.
+   */
+  private subs: Subscription[] = [];
 
   canEdit = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private runSvc: RunStateService) { }
 
   ngOnInit(): void {
+    const subEdit = this.runSvc.getEdit().subscribe(flag => {
+      this.canEdit = flag;
+    });
+
+    this.subs.push(subEdit);
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => {
+      if (sub) {
+        sub.unsubscribe();
+      }
+    });
   }
 
   onClose(): void {
@@ -21,5 +40,6 @@ export class SettingsComponent implements OnInit {
   }
 
   toggleEdit(ev: MatSlideToggleChange): void {
+    this.runSvc.setEdit(ev.checked);
   }
 }
