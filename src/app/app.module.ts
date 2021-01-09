cee0e58f85a779b2f7daf3cdd6ad20c293e0bb1b
Sanjeev Premi <spremi@ymail.com>
2021-01-09 14:07:37 +0530

    feat: /desk-app: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -58,7 +58,12 @@ import { SettingsComponent } from './routes/settings/settings.component';
     MatTabsModule,
     MatToolbarModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: { duration: 2500 },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/routes/desk-app/desk-app.component.ts
+++ src/app/routes/desk-app/desk-app.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DeskApp, DeskConfig } from '@models/desk-config';
+import { initIpcRequest, IpcNg2E } from '@models/ipc-request';
+import { OpStatus } from '@models/op-status';
+import { DataService } from '@services/data.service';
+import { IpcService } from '@services/ipc.service';
+
+const ADD_SUCCESS = 'Desk application added.';
+const ADD_FAILURE = 'Unable to add desk application.';
+
+const MOD_SUCCESS = 'Desk application updated.';
+const MOD_FAILURE = 'Unable to update the desk application.';
+
+const DEL_SUCCESS = 'Desk application deleted.';
+const DEL_FAILURE = 'Unable to delete the desk application.';
 
 @Component({
   selector: 'sp-desk-app',
@@ -6,10 +22,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./desk-app.component.sass'],
 })
 export class DeskAppComponent implements OnInit {
+  app: DeskApp;
 
-  constructor() { }
+  aid: string = null;
+
+  edit = false;
+  delete = false;
+  deleteConfirm = false;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private dataSvc: DataService,
+    private ipcSvc: IpcService,
+    private snackBar: MatSnackBar
+   ) { }
 
   ngOnInit(): void {
+    this.aid = this.route.snapshot.paramMap.get('id');
+
+    this.app = this.dataSvc.getDeskApp(this.aid);
   }
 
+  onDelete(): void {
+    this.delete = true;
+  }
+
+  onDeleteCancel(): void {
+    this.delete = false;
+  }
+
+  onDeleteConfirm(): void {
+    this.deleteConfirm = true;
+
+    const cmd = initIpcRequest(IpcNg2E.APP_DELETE);
+    cmd.reqParams = [ this.aid ];
+
+    this.ipcSvc.send<boolean>(cmd).then(result => {
+      const msg = result ? DEL_SUCCESS : DEL_FAILURE;
+
+      this.snackBar.open(msg, 'DISMISS');
+
+      if (result) {
+        this.router.navigate(['home']);
+      } else {
+        this.delete = false;
+        this.deleteConfirm = false;
+      }
+    }).catch(err => {
+      this.snackBar.open(DEL_FAILURE, 'DISMISS');
+
+      this.delete = false;
+      this.deleteConfirm = false;
+    });
+  }
+
+  onEdit(): void {
+    this.edit = !this.edit;
+  }
+
+  onClose(): void {
+    this.router.navigate(['home']);
+  }
+
+  onStatus(status: OpStatus): void {
+    if (status !== OpStatus.CANCEL) {
+      let msg: string;
+
+      if (this.app) {
+        msg = status === OpStatus.SUCCESS ? MOD_SUCCESS : MOD_FAILURE;
+      } else {
+        msg = status === OpStatus.SUCCESS ? ADD_SUCCESS : ADD_FAILURE;
+      }
+
+      this.snackBar.open(msg, 'DISMISS');
+    }
+
+    this.edit = false;
+  }
 }
