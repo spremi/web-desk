5de4a01462013f62fc64cf15c2c88fcdcc7822f4
Sanjeev Premi <spremi@ymail.com>
2021-01-09 16:31:10 +0530

    refactor: parts: desk-app-view: Update layout
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/desk-app-view/desk-app-view.component.ts
+++ src/app/parts/desk-app-view/desk-app-view.component.ts
@@ -1,8 +1,15 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { DeskApp, DeskAppEvent } from '@models/desk-config';
+import { initIpcRequest, IpcNg2E } from '@models/ipc-request';
 import { IpcService } from '@services/ipc.service';
 import { Subscription } from 'rxjs';
+
+const START_FAILURE = 'Unable to start the desk application.';
+const STOP_FAILURE = 'Unable to stop the desk application.';
+const MINIMIZE_FAILURE = 'Unable to minimize desk application window.';
+const RESTORE_FAILURE = 'Unable to restore desk application window.';
 
 @Component({
   selector: 'sp-desk-app-view',
@@ -11,11 +18,14 @@ import { Subscription } from 'rxjs';
 })
 export class DeskAppViewComponent implements OnInit, OnDestroy {
 
-  @Input() data: DeskApp;
+  @Input() app: DeskApp;
 
   private sub: Subscription;
 
-  constructor(private ipcSvc: IpcService) { }
+  isRunning = false;
+  isVisible = false;
+
+  constructor(private ipcSvc: IpcService, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.sub = this.ipcSvc.getAppEvents().subscribe((ev: DeskAppEvent) => {
@@ -29,17 +39,53 @@ export class DeskAppViewComponent implements OnInit, OnDestroy {
     }
   }
 
-  onOptions(ev: MouseEvent): void {
-    ev.preventDefault();
-    ev.stopPropagation();
+  onStart(): void {
+    const cmd = initIpcRequest(IpcNg2E.APP_LAUNCH);
+    cmd.reqParams = [ this.app.aid ];
 
-    console.log('TODO: Show options.');
+    this.ipcSvc.send<boolean>(cmd).then(result => {
+      if (result) {
+        this.isRunning = true;
+        this.isVisible = true;
+      } else {
+        this.snackBar.open(START_FAILURE, 'DISMISS');
+      }
+    }).catch(err => {
+      this.snackBar.open(START_FAILURE, 'DISMISS');
+    });
   }
 
-  onLaunch(ev: MouseEvent): void {
-    ev.preventDefault();
-    ev.stopPropagation();
+  onStop(): void {
+    const cmd = initIpcRequest(IpcNg2E.WIN_CLOSE);
+    cmd.reqParams = [ this.app.aid ];
 
-    console.log('TODO: Launch application.');
+    this.ipcSvc.send<boolean>(cmd).then(result => {
+      if (result) {
+        this.isRunning = false;
+        this.isVisible = false;
+      } else {
+        this.snackBar.open(STOP_FAILURE, 'DISMISS');
+      }
+    }).catch(err => {
+      this.snackBar.open(STOP_FAILURE, 'DISMISS');
+    });
+  }
+
+  onVisibility(): void {
+    const cmd = initIpcRequest(
+                  this.isVisible ? IpcNg2E.WIN_MINIMIZE : IpcNg2E.WIN_RESTORE);
+    cmd.reqParams = [ this.app.aid ];
+
+    const failMsg = this.isVisible ? MINIMIZE_FAILURE : RESTORE_FAILURE;
+
+    this.ipcSvc.send<boolean>(cmd).then(result => {
+      if (result) {
+        this.isVisible = !this.isVisible;
+      } else {
+        this.snackBar.open(failMsg, 'DISMISS');
+      }
+    }).catch(err => {
+      this.snackBar.open(failMsg, 'DISMISS');
+    });
   }
 }
