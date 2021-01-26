270f67e05e8ff2107046c30ae6a5c819ccfc1af4
Sanjeev Premi <spremi@ymail.com>
2021-01-26 13:08:17 +0530

    feat: app: icon-svc: Implement register()
    Includes definition of commonly used icons
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/services/icon.service.ts
+++ src/app/services/icon.service.ts
@@ -1,9 +1,108 @@
 import { Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { IconInfo, IconSet } from '@models/icon-info';
+
+/**
+ * List of icons used.
+ */
+export const AppIcons: IconSet = {
+  path: 'assets/icons/',
+  list: [
+    {
+      icon: 'add',
+      file: 'add-24px.svg',
+    },
+    {
+      icon: 'anchor',
+      file: 'anchor-24px.svg',
+    },
+    {
+      icon: 'info',
+      file: 'info-24px.svg',
+    },
+    {
+      icon: 'license',
+      file: 'receipt_long-24px.svg',
+    },
+    {
+      icon: 'settings',
+      file: 'settings-24px.svg',
+    },
+    {
+      icon: 'style',
+      file: 'style-24px.svg',
+    },
+  ],
+};
+
+export const ActionIcons: IconSet = {
+  path: 'assets/icons/',
+  list: [
+    {
+      icon: 'close',
+      file: 'close-24px.svg',
+    },
+    {
+      icon: 'delete',
+      file: 'delete-24px.svg',
+    },
+    {
+      icon: 'edit',
+      file: 'create-24px.svg',
+    },
+    {
+      icon: 'max',
+      file: 'expand_less-24px.svg',
+    },
+    {
+      icon: 'min',
+      file: 'expand_more-24px.svg',
+    },
+    {
+      icon: 'start',
+      file: 'play_arrow-24px.svg',
+    },
+    {
+      icon: 'stop',
+      file: 'stop-24px.svg',
+    },
+    {
+      icon: 'view',
+      file: 'article-24px.svg',
+    },
+  ],
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class IconService {
 
-  constructor() { }
+  constructor(
+    private registry: MatIconRegistry,
+    private sanitizer: DomSanitizer) { }
+
+  register(): void {
+    this.registerSet(AppIcons);
+    this.registerSet(ActionIcons);
+  }
+
+  /**
+   * Register icons in the specified set.
+   */
+  private registerSet(icons: IconSet): void {
+    if (icons.list.length === 0) {
+      return;
+    }
+
+    icons.list.forEach((arg: IconInfo) => {
+      const iconPath = (icons.path + '/' + arg.file).replace('//', '/');
+
+      this.registry.addSvgIcon(
+        arg.icon,
+        this.sanitizer.bypassSecurityTrustResourceUrl(iconPath));
+    });
+  }
 }
