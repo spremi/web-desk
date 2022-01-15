be4e8172455e103254bcd265c9eeee4264b04d9a
Sanjeev Premi <spremi@ymail.com>
2022-01-16 00:43:35 +0530

    feat: parts: group: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/group/group.component.ts
+++ src/app/parts/group/group.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DeskGroup } from '@models/desk-config';
 
 @Component({
   selector: 'sp-group',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupComponent implements OnInit {
 
-  constructor() { }
+  @Input() group: DeskGroup;
+
+  isHover = false;
+  isSelected = false;
+
+  constructor(private router: Router) { }
+
+  @HostListener('mouseenter')
+  onMouseEnter(ev: MouseEvent): void {
+    this.isHover = true;
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave(ev: MouseEvent): void {
+    this.isHover = false;
+  }
+
+  @HostBinding('class.hovered')
+  get hovered(): boolean {
+    return this.isHover;
+  }
+
+  @HostBinding('class')
+  get elevation(): string {
+    return this.isHover ? 'mat-elevation-z8' : 'mat-elevation-z1';
+  }
 
   ngOnInit(): void {
   }
 
+  onSelect(): void {
+    this.isSelected = !this.isSelected;
+  }
+
+  onDetails(): void {
+    this.router.navigate(['desk-group', { id: this.group.gid }]);
+  }
 }
