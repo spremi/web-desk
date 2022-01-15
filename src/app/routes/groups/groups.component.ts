d9ee319716db148992240a69bde6c405925e9f6c
Sanjeev Premi <spremi@ymail.com>
2022-01-16 01:11:20 +0530

    feat: /groups: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/routes/groups/groups.component.ts
+++ src/app/routes/groups/groups.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DeskGroup } from '@models/desk-config';
+import { DataService } from '@services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'sp-groups',
@@ -7,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsComponent implements OnInit {
 
-  constructor() { }
+  groups$: Observable<DeskGroup[]>;
+
+  constructor(private dataSvc: DataService) { }
 
   ngOnInit(): void {
+    this.groups$ = this.dataSvc.getDeskGroups();
   }
-
 }
