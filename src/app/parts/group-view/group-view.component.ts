44c2e54b5025e1b504bcc970541e6504f1108ad4
Sanjeev Premi <spremi@ymail.com>
2022-01-16 01:48:03 +0530

    chore: app: Add component 'group-view'
    Command:
      ng g component parts/group-view --style=sass --lintFix=true
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/app/parts/group-view/group-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-group-view',
+  templateUrl: './group-view.component.html',
+  styleUrls: ['./group-view.component.sass'],
+})
+export class GroupViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
