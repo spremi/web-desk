65b1f0db49c5822f4ebed1c3fa6b546d95ceb1c2
Sanjeev Premi <spremi@ymail.com>
2022-02-20 20:00:46 +0530

    feat: parts: desk-app-view: Add/ update group
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/desk-app-edit/desk-app-edit.component.ts
+++ src/app/parts/desk-app-edit/desk-app-edit.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-import { DeskApp } from '@models/desk-config';
+import { DeskApp, DeskGroup } from '@models/desk-config';
 import { initIpcRequest, IpcNg2E, IpcRequest } from '@models/ipc-request';
 import { OpStatus } from '@models/op-status';
 import { DataService } from '@services/data.service';
 import { IpcService } from '@services/ipc.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'sp-desk-app-edit',
@@ -20,10 +21,20 @@ export class DeskAppEditComponent implements OnInit {
 
   appForm: FormGroup;
 
+  group: DeskGroup;
+  groups$: Observable<DeskGroup[]>;
+
   constructor(private ipcSvc: IpcService, private dataSvc: DataService) { }
 
   ngOnInit(): void {
+    this.groups$ = this.dataSvc.getDeskGroups();
+
+    this.group = this.dataSvc.getDeskGroup(this.data.gid);
+
     this.appForm = new FormGroup({
+      group: new FormControl((this.group && this.group.label ? this.group.label : ''), {
+        validators: Validators.required,
+      }),
       label: new FormControl((this.data && this.data.label ? this.data.label : ''), {
         validators: Validators.required,
       }),
@@ -31,6 +42,10 @@ export class DeskAppEditComponent implements OnInit {
         validators: Validators.required,
       }),
     });
+  }
+
+  compareGroups(g1: DeskGroup, g2: DeskGroup): boolean {
+    return (g1 && g2 ? g1.gid === g2.gid : false);
   }
 
   onSubmit(): void {
@@ -49,12 +64,11 @@ export class DeskAppEditComponent implements OnInit {
       if (this.data) {
         cmd = initIpcRequest(IpcNg2E.APP_MODIFY);
 
-        params.push(this.data.gid);
+        params.push(this.appForm.get('group').value);
         params.push(this.data.aid);
         params.push(this.data.seq.toString());
       } else {
         cmd = initIpcRequest(IpcNg2E.APP_CREATE);
-
         params.push(DEFAULT_GID);
       }
 
