a564da76eb1b067020eb5593de70fc20c9180793
Sanjeev Premi <spremi@ymail.com>
2022-04-09 23:24:10 +0530

    feat: svc: run-state: Track group selections
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/services/run-state.service.ts
+++ src/app/services/run-state.service.ts
@@ -24,6 +24,12 @@ export class RunStateService {
   private run$: BehaviorSubject<AppRunAttrs> = new BehaviorSubject<AppRunAttrs>({});
 
   private KEY_EDIT = 'edit';
+  private KEY_SEL_GROUPS = 'sel-groups';
+
+  /**
+   * BehaviorSubject for selected groups.
+   */
+  private selectedGroups$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
   constructor(private ipcSvc: IpcService) { }
 
@@ -37,6 +43,8 @@ export class RunStateService {
     this.edit$.next(this.state.canEdit);
 
     this.run$.next(this.state.runApps);
+
+    this.initSelectedGroups();
   }
 
   /**
@@ -66,6 +74,16 @@ export class RunStateService {
    */
   public getApps(): Observable<AppRunAttrs> {
     return this.run$.asObservable().pipe(
+        filter(o => o !== null),
+        distinctUntilChanged()
+      );
+  }
+
+  /**
+   * Get observable to array of selected application groups.
+   */
+  public getSelectedGroups(): Observable<string[]> {
+    return this.selectedGroups$.asObservable().pipe(
         filter(o => o !== null),
         distinctUntilChanged()
       );
@@ -113,6 +131,47 @@ export class RunStateService {
   }
 
   /**
+   * Add application group to selection.
+   */
+  public addSelectedGroup(gid: string): void {
+    const s = localStorage.getItem(this.KEY_SEL_GROUPS);
+    let g: string[];
+    let m: string[];
+
+    if (s) {
+      g = JSON.parse(s);
+
+      m = [...g, gid];
+    } else {
+      m = [];
+    }
+
+    localStorage.setItem(this.KEY_SEL_GROUPS, JSON.stringify(m));
+
+    this.selectedGroups$.next(m);
+  }
+
+  /**
+   * Remove application group from selection.
+   */
+  public delSelectedGroup(gid: string): void {
+    const s = localStorage.getItem(this.KEY_SEL_GROUPS);
+    let g: string[];
+    let m: string[];
+
+    if (s) {
+      g = JSON.parse(s);
+      m = g.filter(id => id !== gid);
+    } else {
+      m = [];
+    }
+
+    localStorage.setItem(this.KEY_SEL_GROUPS, JSON.stringify(m));
+
+    this.selectedGroups$.next(m);
+  }
+
+  /**
    * Get value of KEY_EDIT from local storage.
    */
   private getFlagEdit(): boolean {
@@ -128,5 +187,21 @@ export class RunStateService {
     const v = (flag === true) ? 'true' : 'false';
 
     localStorage.setItem(this.KEY_EDIT, v);
+  }
+
+  /**
+   * Initialize selected groups from local storage.
+   */
+  private initSelectedGroups(): void {
+    const s = localStorage.getItem(this.KEY_SEL_GROUPS);
+    let g: string[];
+
+    if (s) {
+      g = JSON.parse(s);
+    } else {
+      g = [];
+    }
+
+    this.selectedGroups$.next(g);
   }
 }
