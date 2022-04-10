12811f887d38fbb8a0a38ae33aaf3b658606147c
Sanjeev Premi <spremi@ymail.com>
2022-04-10 18:19:23 +0530

    feat: parts: settings: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/settings/settings.component.ts
+++ src/app/parts/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RunStateService } from '@services/run-state.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'sp-settings',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  canEdit$: Observable<boolean>;
+  viewByGroup$: Observable<boolean>;
+  viewByFilter$: Observable<boolean>;
+
+  constructor(private runSvc: RunStateService) { }
 
   ngOnInit(): void {
+    this.canEdit$ = this.runSvc.getEdit();
+    this.viewByGroup$ = this.runSvc.getViewByGroup();
+    this.viewByFilter$ = this.runSvc.getViewSelectedGroups();
   }
 
+  onToggleEdit(flag: boolean): void {
+    this.runSvc.setEdit(flag);
+  }
+
+  onViewByGroup(flag: boolean): void {
+    this.runSvc.setViewByGroup(flag);
+  }
+
+  onViewByFilter(flag: boolean): void {
+    this.runSvc.setViewSelectedGroups(flag);
+  }
 }
