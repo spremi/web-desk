1de47b9e4722fc6d752a9196a7fad0aa4ce4dda1
Sanjeev Premi <spremi@ymail.com>
2022-04-10 19:35:04 +0530

    feat: /home: Apply filters
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { DeskApp } from '@models/desk-config';
+import { DeskApp, DeskGroup } from '@models/desk-config';
 import { DataService } from '@services/data.service';
-import { Subscription } from 'rxjs';
+import { RunStateService } from '@services/run-state.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'sp-home',
@@ -11,14 +12,30 @@ import { Subscription } from 'rxjs';
 export class HomeComponent implements OnInit, OnDestroy {
   apps: DeskApp[];
 
+  viewByGroup$: Observable<boolean>;
+  viewSelGroups$: Observable<boolean>;
+
+  groups$: Observable<DeskGroup[]>;
+  selGroups$: Observable<string[]>;
+
   private sub: Subscription;
 
-  constructor(private dataSvc: DataService) { }
+  constructor(
+    private dataSvc: DataService,
+    private runSvc: RunStateService
+  ) { }
 
   ngOnInit(): void {
+
+    this.viewByGroup$ = this.runSvc.getViewByGroup();
+    this.viewSelGroups$ = this.runSvc.getViewSelectedGroups();
+    this.selGroups$ = this.runSvc.getSelectedGroups();
+
     this.sub = this.dataSvc.getDeskApps().subscribe(result => {
       this.apps = result;
     });
+
+    this.groups$ = this.dataSvc.getDeskGroups();
   }
 
   ngOnDestroy(): void {
